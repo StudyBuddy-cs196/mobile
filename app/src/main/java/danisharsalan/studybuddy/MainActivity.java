c5b756d8b71a1193f664c4d8e7b2e4abe33208fd
danisharsalan <danisharsalan@yahoo.com>
2016-10-22 14:40:35 -0500

    Added a profile pic imageview by user ur

--- app/src/main/java/danisharsalan/studybuddy/MainActivity.java
+++ app/src/main/java/danisharsalan/studybuddy/MainActivity.java
@@ -2,20 +2,31 @@ package danisharsalan.studybuddy;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import java.io.IOException;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final int RC_SIGN_IN = 0;
     private FirebaseAuth auth;
+    public static String profilePicURL = "";
+    ImageView profilePicSetterImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +45,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     .build(), RC_SIGN_IN);
         }
         findViewById(R.id.logOutButton).setOnClickListener(this);
+        profilePicURL = auth.getCurrentUser().getPhotoUrl()+"";
+        profilePicSetterImageView = (ImageView) findViewById(R.id.profilePicSetter);
+        GetXMLTask task = new GetXMLTask();
+        task.execute(new String[] { profilePicURL });
     }
+
+    private class GetXMLTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap map = null;
+            for (String url : urls) {
+                map = downloadImage(url);
+            }
+            return map;
+        }
+
+        // Sets the Bitmap returned by doInBackground
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            profilePicSetterImageView.setImageBitmap(result);
+        }
+
+        // Creates Bitmap from InputStream and returns it
+        private Bitmap downloadImage(String url) {
+            Bitmap bitmap = null;
+            InputStream stream = null;
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inSampleSize = 1;
+
+            try {
+                stream = getHttpConnection(url);
+                bitmap = BitmapFactory.
+                        decodeStream(stream, null, bmOptions);
+                stream.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        // Makes HttpURLConnection and returns InputStream
+        private InputStream getHttpConnection(String urlString)
+                throws IOException {
+            InputStream stream = null;
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+
+            try {
+                HttpURLConnection httpConnection = (HttpURLConnection) connection;
+                httpConnection.setRequestMethod("GET");
+                httpConnection.connect();
+
+                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    stream = httpConnection.getInputStream();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return stream;
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -43,6 +115,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if(resultCode == RESULT_OK){
                 //User logged in
                 Log.d("AUTH", auth.getCurrentUser().getEmail());
+                Log.d("AUTH", auth.getCurrentUser().getDisplayName());
+                Log.d("AUTH", auth.getCurrentUser().getPhotoUrl()+"");
+                profilePicURL = auth.getCurrentUser().getPhotoUrl()+"";
             } else {
                 //User not authenticated
                 Log.d("AUTH", "NOT AUTHENTICATED");
@@ -59,7 +134,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             Log.d("AUTH", "USER LOGGED OUT!");
-                            finish();
+                            if(auth.getCurrentUser() != null){
+                                //User alread signed in
+                                Log.d("AUTH", auth.getCurrentUser().getEmail());
+                            } else {
+                                startActivityForResult(AuthUI.getInstance()
+                                        .createSignInIntentBuilder()
+                                        .setProviders(
+                                                AuthUI.FACEBOOK_PROVIDER,
+                                                AuthUI.EMAIL_PROVIDER,
+                                                AuthUI.GOOGLE_PROVIDER)
+                                        .build(), RC_SIGN_IN);
+                            }
                         }
                     });
         }
